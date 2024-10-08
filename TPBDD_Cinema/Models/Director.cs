276b32ebd2af0aa445f924fd4d69130f125dd597
promanenko <paihitetty444@gmail.com>
2024-10-08 10:59:27 +0200

    Liaison BDD avec EF #closes 3

---
+++ TPBDD_Cinema/Models/Director.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TPBDD_Cinema.Models;
+
+public partial class Director
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Filmdirector> Filmdirectors { get; set; } = new List<Filmdirector>();
+}
