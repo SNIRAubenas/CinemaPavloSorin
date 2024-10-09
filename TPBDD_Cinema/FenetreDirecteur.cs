00d8a5013e97bfb44856b0cb22f02ca130656f08
spirgari <sorin.pirgari@lyceeastier.com>
2024-10-09 14:38:46 +0200

    DadaGried DirectorFini closes #7

---
+++ TPBDD_Cinema/FenetreDirecteur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPBDD_Cinema
+{
+    public partial class FenetreDirecteur : Form
+    {
+        public FenetreDirecteur()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+        private void FenetreDirecteur_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Supprimer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
